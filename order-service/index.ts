885f873af5f365f36a31138b49dbdcc7e8a9e5f5
Abhisek Behera <babhisek569@gmail.com>
2024-11-18 10:00:00 +0530

    chore(order-service): Implement basic request logging

--- order-service/index.ts
+++ order-service/index.ts
@@ -11,6 +11,7 @@ const app: Application = express();
 app.use(express.json());
 
 // Simple request logger middleware
+// UNIQUE_CHANGE_FOR_COMMIT_37
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();

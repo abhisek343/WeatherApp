50e2c11e46bbe6982a9eb56d5395afc8a6eb9aa4
Abhisek Behera <babhisek569@gmail.com>
2025-01-15 16:00:00 +0530

    refactor(user-service): Add TODO for user profile extension

--- user-service/models/user.ts
+++ user-service/models/user.ts
@@ -14,6 +14,7 @@ const userSchema: Schema<IUser> = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   // TODO: Extend user profile with fields like address, phone number, preferences
+  // UNIQUE_CHANGE_FOR_COMMIT_43
 });
 
 userSchema.pre<IUser>("save", async function (this: IUser, next: (err?: Error) => void) {
